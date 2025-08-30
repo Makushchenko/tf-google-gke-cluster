953da0fdf2b2b61f3d3ccbd5b611012e3bd72672
Makushchenko <sink.ice1@gmail.com>
2025-08-30 16:21:05 +0000

    DEVOPS-72  #comment DEVOPS-72 Fixed wrong link on the cluster resourse

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "endpoint_dns" {
   description = "Cluster endpoint DNS"
-  value       = google_container_cluster.main.control_plane_endpoints_config[0].dns_endpoint_config[0].endpoint
+  value       = google_container_cluster.this.control_plane_endpoints_config[0].dns_endpoint_config[0].endpoint
   depends_on = [
-    google_container_cluster.main
+    google_container_cluster.this
   ]
 }
 
